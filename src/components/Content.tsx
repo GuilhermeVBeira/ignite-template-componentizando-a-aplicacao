6864825c4bbaf872af085732689450f100d1970f
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2021-04-03 12:18:13 -0300

    Challenge finalized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import React, {useEffect, useState} from 'react';
+import {api} from '../services/api';
+import {MovieCard} from './MovieCard';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
 }
+
+interface ContentProps {
+    selectedGenreId: number
+}
+
+export const Content:React.FC<ContentProps> = ({selectedGenreId}) => {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  useEffect(() => {
+
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId]);
+
+    return (
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+    )
+}
+
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import React, {useEffect, useState} from 'react';
+import {api} from '../services/api';
+import {Button} from './Button';
+
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+    handleClickButton(genderId: number): void,
+    selectedGenreId: number
+}
+
+export const SideBar:React.FC<SideBarProps> = ({handleClickButton, selectedGenreId}) => {
+    const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+    useEffect(() => {
+        api.get<GenreResponseProps[]>('genres').then(response => {
+        setGenres(response.data);
+        });
+    }, []);
+    return (
+        <div className="buttons-container">
+            {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+      ))}
+    </div>
+)
 }
